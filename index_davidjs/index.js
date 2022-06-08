12726da1c1373fc19402807cdfcdf204f788fa8d
Santiturneo <santiagoturneo@gmail.com>
2022-06-08 11:28:10 -0500

    funcion suma

--- index_davidjs/index.js
+++ index_davidjs/index.js
@@ -1,3 +1,12 @@
-console.log("mis capacidades y mis hablidades")
+var nu1=5
+var nu2=54
 
-console.log("mis habilidades son  html,css,js,python ")
+function suma(n,n1) {
+
+    var suma=n+n1
+    return suma
+    
+}
+
+
+console.log(suma(nu1,nu2))
